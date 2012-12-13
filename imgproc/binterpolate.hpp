0ca6d03577ac0582fea089614b7d9aaa6f9c6411
Václav Blažek <vaclav.blazek@citationtech.net>
2012-12-13 18:15:27 +0000

    shared/imgproc: fixed bilinear interpolation, added insideImage() func
    git-svn-id: https://scm.citationtech.net/trunk/shared/imgproc/imgproc@2671 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- imgproc/binterpolate.hpp
+++ imgproc/binterpolate.hpp
@@ -9,37 +9,44 @@
 #define imgproc_binterpolate_hpp_included_
 
 #include <cmath>
+#include <fstream>
 #include <math/geometry_core.hpp>
+
 
 namespace imgproc {
 
 namespace gil = boost::gil;
 
 namespace detail {
-    inline int clamp(int value, int max) {
-        if (value < 0) { return 0; }
-        if (value > max) { return max; }
+    template <typename T, typename Q>
+    inline T clamp(T value, Q max) {
+        if (value < T(0)) { return T(0); }
+        if (value > T(max)) { return T(max); }
         return value;
     }
 
     inline math::Point2 fitIn01(math::Point2 p) {
-        if (p(0) < 0.) {
-            p(0) = 0.;
-        } else if (p(0) > 1.) {
-            p(0) = 1.;
-        }
-
-        if (p(1) < 0.) {
-            p(1) = 0.;
-        } else if (p(1) > 1.) {
-            p(1) = 1.;
-        }
+        p(0) = clamp(p(0), 1.);
+        p(1) = clamp(p(1), 1.);
         return p;
     }
 } // namespace detail
 
 
-/** NB: Adds interpolated values to result. Set result to zero prior calling
+template <typename T, typename Q>
+bool insideImage(T px, T py, Q width, Q height)
+{
+    return ((px >= T(0)) && (px <= T(width - 1))
+            && (py >= T(0)) && (py <= T(height - 1)));
+}
+
+template <typename T, typename Q>
+bool insideImage(const math::Point2_<T> &point, const math::Size2_<Q> &size)
+{
+    return insideImage(point(0), point(1), size.width, size.height);
+}
+
+/** NB: Adds interpolated values to the result. Set result to zero prior calling
  *  this function if this behaviour is not desired.
  */
 template <typename T, typename Q, typename R>
@@ -57,7 +64,7 @@ void bilinearInterpolate(const T *data, size_t pitch, size_t width
     for (int m(0); m <= 1; ++m) {
         for (int n(0); n <= 1; ++n) {
             int u(detail::clamp(floor(0), width - 1) + m);
-            int v(detail::clamp(floor(0), height - 1) + n);
+            int v(detail::clamp(floor(1), height - 1) + n);
 
             size_t offset(v * pitch + u * channels);
             double s(fabs(1 - m - dpoint(0)) * fabs(1 - n - dpoint(1)));
