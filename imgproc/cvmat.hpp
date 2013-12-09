340c9e2bfd41b877a4a3a114912ed753db858dab
Václav Blažek <vaclav.blazek@citationtech.net>
2013-12-09 20:34:08 +0000

    shared/imgproc: updated matrix/view transformations
    git-svn-id: https://scm.citationtech.net/trunk/shared/imgproc/imgproc@7670 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- imgproc/cvmat.hpp
+++ imgproc/cvmat.hpp
@@ -3,55 +3,115 @@
 
 #include <opencv2/core/mat.hpp>
 
-#include "error.hpp"
+#include "utility/raise.hpp"
 
+#include "error.hpp"
 #include "gil-float-image.hpp"
 
 namespace imgproc {
 
 namespace gil = boost::gil;
 
-template <typename PixelType>
-typename gil::image<PixelType, false>::view_t view(cv::Mat &m)
-{
-    // TODO: check matrix type/channels
+namespace detail {
 
+template <typename PixelType>
+inline void checkMatrixChannels(const cv::Mat &m)
+{
+    if (gil::num_channels<PixelType>::value != m.channels()) {
+        utility::raise<imgproc::TypeError>
+            ("Cannot create gil view from OpenCV matrix: matrix "
+             "has %d channels but view will have %d channels."
+             , m.channels(), gil::num_channels<PixelType>::value);
+    }
+}
+
+inline const char* cvTypeName(int depth) {
+    switch (depth) {
+    case CV_8U: return "unsigned char";
+    case CV_8S: return "char/signed char";
+    case CV_16U: return "unsigned short";
+    case CV_16S: return "signed short";
+    case CV_32S: return "int";
+    case CV_32F: return "float";
+    case CV_64F: return "double";
+    };
+    utility::raise<imgproc::TypeError>
+        ("Matrix has unknown value of depth: %s", depth);
+    throw;
+}
+
+template <typename T> void checkMatrixElement(const cv::Mat &m);
+
+#define GENERATE_CHECK_MATRIX_ELEMENT(TYPE, CV_DEPTH)                   \
+    template <>                                                         \
+    inline void checkMatrixElement<TYPE>(const cv::Mat &m) {            \
+        if (m.depth() != CV_DEPTH) {                                    \
+            utility::raise<imgproc::TypeError>                          \
+                ("Cannot create gil view from OpenCV matrix: matrix "   \
+                 "has different element type (expected %s, got %s)."    \
+                 , #TYPE, cvTypeName(m.depth()));                       \
+        }                                                               \
+    }
+
+GENERATE_CHECK_MATRIX_ELEMENT(unsigned char, CV_8U)
+GENERATE_CHECK_MATRIX_ELEMENT(char, CV_8S)
+GENERATE_CHECK_MATRIX_ELEMENT(signed char, CV_8S)
+GENERATE_CHECK_MATRIX_ELEMENT(unsigned short, CV_16U)
+GENERATE_CHECK_MATRIX_ELEMENT(signed short, CV_16S)
+GENERATE_CHECK_MATRIX_ELEMENT(signed int, CV_32S)
+GENERATE_CHECK_MATRIX_ELEMENT(float, CV_32F)
+GENERATE_CHECK_MATRIX_ELEMENT(double, CV_64F)
+
+#undef GENERATE_CHECK_MATRIX_ELEMENT
+
+template <typename PixelType>
+inline void checkMatrix(const cv::Mat &m)
+{
+    checkMatrixChannels<PixelType>(m);
+    checkMatrixElement<typename gil::channel_type<PixelType>::type>(m);
+}
+
+} // namespace detail
+
+template <typename PixelType>
+inline typename gil::image<PixelType, false>::view_t view(cv::Mat &m)
+{
+    detail::checkMatrix<PixelType>(m);
     return gil::interleaved_view
-        (m.rows, m.cols
+        (m.cols, m.rows
          , reinterpret_cast<PixelType*>(m.data)
          , m.step);
 }
 
 template <typename PixelType>
-typename gil::image<PixelType, false>::const_view_t view(const cv::Mat &m)
+inline typename gil::image<PixelType, false>::const_view_t
+view(const cv::Mat &m)
 {
-    // TODO: check matrix type/channels
-
+    detail::checkMatrix<PixelType>(m);
     return gil::interleaved_view
-        (m.rows, m.cols
+        (m.cols, m.rows
          , reinterpret_cast<const PixelType*>(m.data)
          , m.step);
 }
 
 template <typename PixelType>
-typename gil::image<PixelType, false>::const_view_t const_view(cv::Mat &m)
+inline typename gil::image<PixelType, false>::const_view_t
+const_view(cv::Mat &m)
 {
-    // TODO: check matrix type/channels
-
+    detail::checkMatrix<PixelType>(m);
     return gil::interleaved_view
-        (m.rows, m.cols
+        (m.cols, m.rows
          , reinterpret_cast<const PixelType*>(m.data)
          , m.step);
 }
 
 template <typename PixelType>
-typename gil::image<PixelType, false>::const_view_t
+inline typename gil::image<PixelType, false>::const_view_t
 const_view(const cv::Mat &m)
 {
-    // TODO: check matrix type/channels
-
+    detail::checkMatrix<PixelType>(m);
     return gil::interleaved_view
-        (m.rows, m.cols
+        (m.cols, m.rows
          , reinterpret_cast<const PixelType*>(m.data)
          , m.step);
 }
