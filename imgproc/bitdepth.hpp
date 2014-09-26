1aac05c4a37dab3328d3988cc696c5db93ccd5d8
Ladislav Horký <ladislav.horky@citationtech.net>
2014-09-26 16:27:27 +0000

    vadstena: window-flow diagnostic output handles 16bit images.
    git-svn-id: https://scm.citationtech.net/trunk/shared/imgproc/imgproc@11156 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ imgproc/bitdepth.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file bitdept.hpp
+ * @author Ladislav Horky <ladislav.horky@citationtech.net>
+ *
+ * Bit depth conversions
+ */
+
+#ifndef IMGPROC_BITDEPTH_HPP
+#define IMGPROC_BITDEPTH_HPP
+
+#include <opencv2/core/core.hpp>
+#include "dbglog/dbglog.hpp"
+
+namespace imgproc {
+
+cv::Mat get8BitImage( const cv::Mat image ) {
+    cv::Mat result8bit;
+    
+    if ( image.depth() == CV_16U ) {
+        
+        image.convertTo( result8bit, CV_8U, 0.00390625 );
+    } else if ( image.depth() == CV_8U ) {
+        
+        result8bit = image.clone();
+    } else {
+        
+        LOGTHROW(err2, std::runtime_error) 
+                << "Only 16bit->8bit conversion implemented!";
+    }
+    
+    return result8bit;
+}
+
+} // namespace imgproc
+
+#endif // IMGPROC_COLOR_HPP
