ed765ba3ff2c31db11e6cae79f05ff6cea7b4d12
Ladislav Horký <ladislav.horky@citationtech.net>
2015-07-22 11:20:07 +0000

    imgproc: fixed raster mask dumping for whole-number pixelSize
    git-svn-id: https://scm.citationtech.net/trunk/shared/imgproc/imgproc@13912 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- imgproc/rastermask/cvmat.cpp
+++ imgproc/rastermask/cvmat.cpp
@@ -54,14 +54,26 @@ cv::Mat asCvMat(const RasterMask &mask, double pixelSize)
               , CV_8UC1);
     m = cv::Scalar(0);
     auto white(cv::Scalar(0xff));
+    
+    /* NB Tomas D. fixed calculation of end in a way it stopped working for 
+     * pixelSize being whole number.
+     * 
+     * Fixme: fixed so that it works for whole numbers again while retaining 
+     * Tomas D.'s functinality. However, this should be fixed by removing 
+     * pixelSize entirely - possible scaling must be done outside because for 
+     * rational pixelSize the rasterizing operation as implemented now is not 
+     * well defined (there are overlaps of cells) 
+     */ 
+    
+    uint fracAdj(std::round(pixelSize) - pixelSize != 0.0 ? 1 : 0);
 
     mask.forEachQuad([&](uint xstart, uint ystart, uint xsize
                          , uint ysize, bool)
     {
         cv::Point2i start(int(std::floor(pixelSize * xstart))
                           , int(std::floor(pixelSize * ystart)));
-        cv::Point2i end(int(std::ceil(pixelSize * (xstart + xsize - 1 )))
-                        , int(std::ceil(pixelSize * (ystart + ysize - 1))));
+        cv::Point2i end(int(std::ceil(pixelSize * (xstart + xsize - fracAdj )))
+                        , int(std::ceil(pixelSize * (ystart + ysize - fracAdj))));
 
         cv::rectangle(m, start, end, white, CV_FILLED, 4);
     }, RasterMask::Filter::white);
