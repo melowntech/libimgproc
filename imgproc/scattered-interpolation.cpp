98b7085a0c4f9b438e505526f10385a297b032d7
Jakub Cerveny <jakub.cerveny@melown.com>
2016-10-04 11:20:56 +0200

    scattered-interpolation: improved documentation

--- imgproc/scattered-interpolation.cpp
+++ imgproc/scattered-interpolation.cpp
@@ -11,13 +11,7 @@ namespace imgproc {
 typedef Eigen::SparseMatrix<double> SparseMatrix;
 typedef Eigen::Triplet<double> Triplet;
 
-/** Solves the boundary value problem -\Delta u = 0 on elements in the matrix
- *  'data' that correspond to unset elements in 'mask'. Elements corresponding
- *  to set positions in 'mask' are regarded as given data.
- *
- *  The method is described in section 3.8 "Laplace Interpolation" of
- *  Numerical Recipes in C, Third Edition.
- */
+
 void laplaceInterpolate(cv::Mat &data, const imgproc::RasterMask &mask)
 {
     int w = data.cols, h = data.rows;
--- imgproc/scattered-interpolation.hpp
+++ imgproc/scattered-interpolation.hpp
@@ -14,13 +14,16 @@
 
 namespace imgproc {
 
-
-/**
+/** Solves the boundary value problem -\Delta u = 0 on elements in the matrix
+ *  'data' that correspond to unset elements in 'mask'. Elements corresponding
+ *  to set positions in 'mask' are regarded as given data.
  *
+ *  The method is described in section 3.8 "Laplace Interpolation" of
+ *  Numerical Recipes in C, Third Edition.
+ *
+ *  NOTE: 'data' must be of type CV_32F.
  */
 void laplaceInterpolate(cv::Mat &data, const imgproc::RasterMask &mask);
-
-
 
 } // imgproc
 
