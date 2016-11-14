e96a642353e0847dbbaff63cc1dce67cf880a226
Vaclav Blazek <vaclav.blazek@citationtech.net>
2016-11-14 17:02:08 +0100

    png: file serialization support

--- imgproc/png.cpp
+++ imgproc/png.cpp
@@ -1,5 +1,7 @@
+#include <cstdio>
 #include <memory>
 #include <algorithm>
+#include <system_error>
 
 #include <png.h>
 
@@ -7,6 +9,8 @@
 
 #include "./png.hpp"
 #include "./error.hpp"
+
+namespace fs = boost::filesystem;
 
 namespace imgproc { namespace png {
 
@@ -45,6 +49,20 @@ public:
                            , imgproc_PngFlush);
     }
 
+    PngWriter(FILE *out)
+        : png_(::png_create_write_struct
+               (PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr))
+        , info_()
+    {
+        if (!png_) {
+            LOGTHROW(err1, Error)
+                << "Unable to initialize PNG writer.";
+        }
+        info_ = ::png_create_info_struct(png_);
+
+        ::png_init_io(png_, out);
+    }
+
     ~PngWriter() {
         png_destroy_write_struct(&png_, &info_);
     }
@@ -58,12 +76,9 @@ private:
 };
 
 template <typename PixelType, typename ConstView>
-SerializedPng serializeView(const ConstView &view, int compressionLevel
-                   , int type)
+void writeView(PngWriter &writer, const ConstView &view
+               , int compressionLevel, int type)
 {
-    SerializedPng out;
-    PngWriter writer(out);
-
     ::png_set_IHDR(writer.png(), writer.info(), view.width(), view.height()
                    , 8, type, PNG_INTERLACE_NONE
                    , PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
@@ -84,8 +99,50 @@ SerializedPng serializeView(const ConstView &view, int compressionLevel
     }
 
     ::png_write_end(writer.png(), writer.info());
+}
 
+template <typename PixelType, typename ConstView>
+SerializedPng serializeView(const ConstView &view, int compressionLevel
+                            , int type)
+{
+    SerializedPng out;
+    PngWriter writer(out);
+    writeView<PixelType>(writer, view, compressionLevel, type);
     return out;
+}
+
+template <typename PixelType, typename ConstView>
+void writeViewToFile(const fs::path &path, const ConstView &view
+                     , int compressionLevel, int type)
+{
+    struct FileHolder {
+        FileHolder(const fs::path &path)
+            : path(path), f(std::fopen(path.c_str(), "w"))
+        {}
+
+        ~FileHolder() {
+            if (!f) { return; }
+            if (std::fclose(f)) {
+                std::system_error e(errno, std::system_category());
+                LOG(warn3) << "Cannot close PNG file " << path << ": <"
+                           << e.code() << ", " << e.what() << ">.";
+            }
+        }
+
+        const fs::path path;
+        std::FILE *f;
+    };
+
+    FileHolder fh(path);
+    if (!fh.f) {
+        std::system_error e(errno, std::system_category());
+        LOG(err3) << "Cannot create PNG file " << path << ": <"
+                   << e.code() << ", " << e.what() << ">.";
+        throw e;
+    }
+
+    PngWriter writer(fh.f);
+    writeView<PixelType>(writer, view, compressionLevel, type);
 }
 
 } // namespace
@@ -97,6 +154,15 @@ SerializedPng serialize(const boost::gil::gray8_image_t &image
         (boost::gil::const_view(image), compressionLevel, PNG_COLOR_TYPE_GRAY);
 }
 
+void write(const fs::path &file
+           , const boost::gil::gray8_image_t &image
+           , int compressionLevel)
+{
+    return writeViewToFile<boost::gil::gray8_pixel_t>
+        (file, boost::gil::const_view(image)
+         , compressionLevel, PNG_COLOR_TYPE_GRAY);
+}
+
 SerializedPng serialize(const boost::gil::rgb8_image_t &image
                         , int compressionLevel)
 {
@@ -104,11 +170,30 @@ SerializedPng serialize(const boost::gil::rgb8_image_t &image
         (boost::gil::const_view(image), compressionLevel, PNG_COLOR_TYPE_RGB);
 }
 
+void write(const fs::path &file
+           , const boost::gil::rgb8_image_t &image
+           , int compressionLevel)
+{
+    return writeViewToFile<boost::gil::rgb8_pixel_t>
+        (file, boost::gil::const_view(image)
+         , compressionLevel, PNG_COLOR_TYPE_RGB);
+}
+
 SerializedPng serialize(const boost::gil::rgba8_image_t &image
                         , int compressionLevel)
 {
     return serializeView<boost::gil::rgba8_pixel_t>
         (boost::gil::const_view(image), compressionLevel, PNG_COLOR_TYPE_RGBA);
+}
+
+
+void write(const fs::path &file
+           , const boost::gil::rgba8_image_t &image
+           , int compressionLevel)
+{
+    return writeViewToFile<boost::gil::rgba8_pixel_t>
+        (file, boost::gil::const_view(image)
+         , compressionLevel, PNG_COLOR_TYPE_RGBA);
 }
 
 } } // namespace imgproc::png
--- imgproc/png.hpp
+++ imgproc/png.hpp
@@ -1,6 +1,7 @@
 #ifndef imgproc_png_hpp_included_
 #define imgproc_png_hpp_included_
 
+#include <boost/filesystem/path.hpp>
 #include <boost/gil/gil_all.hpp>
 
 namespace imgproc { namespace png {
@@ -33,6 +34,36 @@ SerializedPng serialize(const boost::gil::rgb8_image_t &image
  */
 SerializedPng serialize(const boost::gil::rgba8_image_t &image
                         , int compressionLevel = -1);
+
+/** Write grayscale GIL image into PNG file.
+ *
+ * \param image image to serialize
+ * \param compressionLevel 0-9, other values map to default (whatever it is)
+ * \return serialized image
+ */
+void write(const boost::filesystem::path &file
+           , const boost::gil::gray8_image_t &image
+           , int compressionLevel = -1);
+
+/** Write RGB GIL image into PNG file.
+ *
+ * \param image image to serialize
+ * \param compressionLevel 0-9, other values map to default (whatever it is)
+ * \return serialized image
+ */
+void write(const boost::filesystem::path &file
+           , const boost::gil::rgb8_image_t &image
+           , int compressionLevel = -1);
+
+/** Write RGBA GIL image into PNG file.
+ *
+ * \param image image to serialize
+ * \param compressionLevel 0-9, other values map to default (whatever it is)
+ * \return serialized image
+ */
+void write(const boost::filesystem::path &file
+           , const boost::gil::rgba8_image_t &image
+           , int compressionLevel = -1);
 
 } } // namespace imgproc::png
 
