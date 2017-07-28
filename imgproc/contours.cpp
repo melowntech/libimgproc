acad2ce39e8d18fc3d5f979c474f07338e4c2eea
Vaclav Blazek <vaclav.blazek@citationtech.net>
2017-07-28 19:41:14 +0200

    better data structures (using boost multi index container); contour not beveled at image border

--- imgproc/contours.cpp
+++ imgproc/contours.cpp
@@ -40,170 +40,325 @@
 
 #include "./contours.hpp"
 
+namespace bmi = boost::multi_index;
+
 namespace imgproc {
 
 namespace {
 
-typedef int VertexIndex;
-typedef int SegmentIndex;
-
 typedef math::Point2i Vertex;
 typedef math::Points2i Vertices;
 
+struct Segment;
+
+struct Ring {
+    typedef std::shared_ptr<Ring> pointer;
+
+    Ring(const Segment *head) : head(head) {}
+
+    const Segment *head;
+
+    typedef std::set<const Ring*> set;
+};
+
 struct Segment {
+    std::uint8_t fullType;
     std::uint8_t type;
     Vertex start;
     Vertex end;
-    SegmentIndex next;
-    SegmentIndex prev;
-    SegmentIndex ring;
 
-    Segment(std::uint8_t type, Vertex start, Vertex end, SegmentIndex ring)
-        : type(type), start(start), end(end), next(-1), prev(-1), ring(ring)
+    struct Links {
+        const Segment *prev;
+        const Segment *next;
+        Ring::pointer ring;
+
+        Links(const Segment *prev, const Segment *next)
+            : prev(prev), next(next)
+        {}
+    };
+
+    mutable Links links;
+
+    Segment(std::uint8_t fullType, std::uint8_t type
+            , Vertex start, Vertex end
+            , const Segment *prev, const Segment *next)
+        : fullType(fullType), type(type), start(start), end(end)
+        , links(prev, next)
     {}
-
-    typedef std::vector<Segment> list;
-    typedef std::map<Vertex, SegmentIndex> map;
 };
 
+struct StartIdx {};
+struct EndIdx {};
+
+typedef boost::multi_index_container<
+    Segment
+    , bmi::indexed_by<
+          bmi::ordered_unique<
+              bmi::tag<StartIdx>
+              , BOOST_MULTI_INDEX_MEMBER
+              (Segment, decltype(Segment::start), start)
+              >
+          , bmi::ordered_unique<
+                bmi::tag<EndIdx>
+                , BOOST_MULTI_INDEX_MEMBER
+                (Segment, decltype(Segment::end), end)
+                >
+          >
+    > SegmentMap;
 
 struct ContourBuilder {
-    SegmentIndex findSegment(const Segment::map &where, const Vertex &v)
-        const
-    {
-        auto fwhere(where.find(v));
-        return ((fwhere == where.end()) ? -1 : fwhere->second);
+    template <typename Index>
+    const Segment* find(Index &idx, const Vertex &v) {
+        auto fidx(idx.find(v));
+        return ((fidx == idx.end()) ? nullptr : &*fidx);
     }
 
-    Segment* get(SegmentIndex si) {
-        if (si < 0) { return nullptr; }
-        return &segments[si];
+    const Segment* findByStart(const Vertex &v) {
+        return find(segments.get<StartIdx>(), v);
     }
 
-    Segment* getPrev(const Segment &s) { return get(s.prev); }
-    Segment* getNext(const Segment &s) { return get(s.next); }
-
-    void commonRing(Segment &s1, Segment &s2)
-    {
-        if (s1.ring < s2.ring) {
-            auto *current(&s2);
-            do {
-                current->ring = s1.ring;
-            } while ((current = getNext(*current)));
-        } else if (s2.ring < s1.ring) {
-            auto *current(&s1);
-            do {
-                current->ring = s2.ring;
-            } while ((current = getPrev(*current)));
-        }
+    const Segment* findByEnd(const Vertex &v) {
+        return find(segments.get<EndIdx>(), v);
     }
 
-    void addSegment(std::uint8_t type, const Vertex &start, const Vertex &end)
+    Ring::pointer newRing(const Segment *head) {
+        Ring::pointer ring(new Ring(head)
+                           , [&](Ring *ring) {
+                               rings.erase(ring);
+                               delete ring;
+                           });
+
+        rings.insert(ring.get());
+        return ring;
+    }
+
+    void addSegment(std::uint8_t fullType, std::uint8_t type
+                    , const Vertex &start, const Vertex &end)
     {
-        // make room for new segment
-        if (segments.capacity() == segments.size()) {
-            segments.reserve(2 * segments.size());
-        }
+        auto *prev(findByEnd(start));
+        auto *next(findByStart(end));
 
-        // find indices of previous and next segments
-        auto prevIndex(findSegment(segmentsByEnd, start));
-        auto nextIndex(findSegment(segmentsByStart, end));
+        const auto &s
+            (*segments.insert(Segment(fullType, type, start, end
+                                      , prev, next)).first);
 
-        auto *prev(get(prevIndex));
-        auto *next(get(nextIndex));
+        // LOG(info4) << "Segment " << s.start << " -> " << s.end << "> "
+        //            << &s;
 
-        // create segment
-        const auto si(segments.size());
-        segments.emplace_back(type, start, end, si);
-        auto &s(segments.back());
+        // stranded segment -> we're done here
+        if (!prev && !next) { return; }
 
-        bool startRecorded(false);
-        bool endRecorded(false);
+        // insert new segment
 
-        // link previous segment
+        // link
+        Ring::pointer pRing;
+        Ring::pointer nRing;
         if (prev) {
-            // linke there
-            s.prev = prevIndex;
-            // and back
-            prev->next = si;
-            // use common ring
-            commonRing(*prev, s);
+            prev->links.next = &s;
+            pRing = prev->links.ring;
         }
-
-        // link next segment
         if (next) {
-            // link there
-            s.next = nextIndex;
-            // and back
-            next->prev = si;
-            // use common ring
-            commonRing(s, *next);
+            next->links.prev = &s;
+            nRing = next->links.ring;
         }
 
-        // remember segment by point
-        if (!startRecorded) {
-            segmentsByStart.insert(Segment::map::value_type(s.start, si));
-        }
+        const auto distributeRingPrev([&](const Segment *s) -> void
+        {
+            // grab current ring, move skip current and write ring to all
+            // previous segments
+            const auto &ring(s->links.ring);
+            for (s = s->links.prev; s; s = s->links.prev) {
+                s->links.ring = ring;
+            }
+        });
 
-        if (!endRecorded) {
-            segmentsByEnd.insert(Segment::map::value_type(s.end, si));
+        const auto distributeRingNext([&](const Segment *s) -> void
+        {
+            // grab current ring, move skip current and write ring to all
+            // next segments
+            const auto &ring(s->links.ring);
+            for (s = s->links.next; s; s = s->links.next) {
+                s->links.ring = ring;
+            }
+        });
+
+        // unify/create ring
+        if (!pRing && !nRing) {
+            // no ring, create for this segment
+            s.links.ring = newRing(&s);
+            // and copy to others (there is at most one segment in each
+            // direction -> simple assignment)
+            if (next) { next->links.ring = s.links.ring; }
+            if (prev) { prev->links.ring = s.links.ring; }
+        } else if (!pRing) {
+            // distribute ring from next node to all its previous segments
+            distributeRingPrev(next);
+        } else if (!nRing) {
+            // distribute ring from prev node to all its next segments
+            distributeRingNext(prev);
+        } else if (pRing != nRing) {
+            // both valid but different, prefer prev segment
+            distributeRingNext(prev);
+        } else {
+            // both are the same, just assing
+            s.links.ring = pRing;
         }
     }
 
     void add(int x, int y, std::uint8_t type) {
-        // LOG(info4) << "(" << x << ", " << y << "): " << std::bitset<4>(type);
-
         // 2x scale to get rid of non-integral values
         x *= 2;
         y *= 2;
 
+        // LOG(info4) << "Adding inner: " << x << ", " << y;
+
         switch (type) {
         case 0x0: return;
 
-        case 0x1: return addSegment(type, { x, y + 1 }, { x + 1, y + 2 });
-        case 0x2: return addSegment(type, { x + 1, y + 2 }, { x + 2, y + 1 });
-        case 0x3: return addSegment(type, { x, y + 1 }, { x + 2, y + 1 });
-        case 0x4: return addSegment(type, { x + 2, y + 1 }, { x + 1, y });
-        case 0x5:
-            addSegment(0x7, { x, y + 1 }, { x + 1, y });
-            return addSegment(0xd, { x + 2, y + 1 }, { x + 1, y + 2 });
-        case 0x6: return addSegment(type, { x + 1, y + 2 }, { x + 1, y });
-        case 0x7: return addSegment(type, { x, y + 1 }, { x + 1, y });
-        case 0x8: return addSegment(type, { x + 1, y }, { x, y + 1 });
-        case 0x9: return addSegment(type, { x + 1, y }, { x + 1, y + 2 });
+        case 0x1:
+            return addSegment(type, type, { x, y + 1 }, { x + 1, y + 2 });
+
+        case 0x2:
+            return addSegment(type, type, { x + 1, y + 2 }, { x + 2, y + 1 });
+
+        case 0x3:
+            return addSegment(type, type, { x, y + 1 }, { x + 2, y + 1 });
+
+        case 0x4:
+            return addSegment(type, type, { x + 2, y + 1 }, { x + 1, y });
+
+        case 0x5: // 0x7 + 0xd
+            addSegment(type, 0x7, { x, y + 1 }, { x + 1, y });
+            return addSegment(type, 0xd, { x + 2, y + 1 }, { x + 1, y + 2 });
+
+        case 0x6:
+            return addSegment(type, type, { x + 1, y + 2 }, { x + 1, y });
+
+        case 0x7:
+            return addSegment(type, type, { x, y + 1 }, { x + 1, y });
+
+        case 0x8:
+            return addSegment(type, type, { x + 1, y }, { x, y + 1 });
+
+        case 0x9:
+            return addSegment(type, type, { x + 1, y }, { x + 1, y + 2 });
+
         case 0xa: // 0xb + 0xe
-            addSegment(0xb, { x + 1, y }, { x + 2, y + 1 });
-            return addSegment(0xe, { x + 1, y + 2 }, { x, y + 1 });
-        case 0xb: return addSegment(type, { x + 1, y }, { x + 2, y + 1 });
-        case 0xc: return addSegment(type, { x + 2, y + 1 }, { x, y + 1 });
-        case 0xd: return addSegment(type, { x + 2, y + 1 }, { x + 1, y + 2 });
-        case 0xe: return addSegment(type, { x + 1, y + 2 }, { x, y + 1 });
+            addSegment(type, 0xb, { x + 1, y }, { x + 2, y + 1 });
+            return addSegment(type, 0xe, { x + 1, y + 2 }, { x, y + 1 });
+
+        case 0xb:
+            return addSegment(type, type, { x + 1, y }, { x + 2, y + 1 });
+
+        case 0xc:
+            return addSegment(type, type, { x + 2, y + 1 }, { x, y + 1 });
+
+        case 0xd:
+            return addSegment(type, type, { x + 2, y + 1 }, { x + 1, y + 2 });
+
+        case 0xe:
+            return addSegment(type, type, { x + 1, y + 2 }, { x, y + 1 });
+
         case 0xf: return;
         }
     }
 
-    math::Points2 extract(SegmentIndex start) {
-        LOG(info4) << "Processing ring: " << start;
+    void addBorder(int x, int y, std::uint8_t type) {
+        // 2x scale to get rid of non-integral values
+        x *= 2;
+        y *= 2;
+
+        // LOG(info4) << "Adding border: " << x << ", " << y;
+
+        switch (type) {
+        case 0x0: return;
+
+        case 0x1:
+            addSegment(type, 0x3, { x, y + 1 }, { x + 1, y + 1 });
+            return addSegment(type, 0x9, { x + 1, y + 1 }, { x + 1, y + 2 });
+
+        case 0x2:
+            addSegment(type, 0x6, { x + 1, y + 2 }, { x + 1, y + 1 });
+            return addSegment(type, 0x3, { x + 1, y + 1 }, { x + 2, y + 1 });
+
+        case 0x3:
+            return addSegment(type, type, { x, y + 1 }, { x + 2, y + 1 });
+
+        case 0x4:
+            addSegment(type, 0xc, { x + 2, y + 1 }, { x + 1, y + 1 });
+            return addSegment(type, 0x6, { x + 1, y + 1 }, { x + 1, y });
+
+        case 0x5: // 0x7 + 0xd
+            addSegment(type, 0x6, { x, y + 1 }, { x, y });
+            addSegment(type, 0x3, { x, y }, { x + 1, y });
+            addSegment(type, 0x9, { x + 2, y + 1 }, { x + 2, y + 2 });
+            return addSegment(type, 0xc, { x + 2, y + 2 }, { x + 1, y + 2 });
+
+        case 0x6:
+            return addSegment(type, type, { x + 1, y + 2 }, { x + 1, y });
+
+        case 0x7:
+            addSegment(type, 0x6, { x, y + 1 }, { x, y });
+            return addSegment(type, 0x3, { x, y }, { x + 1, y });
+
+        case 0x8:
+            addSegment(type, 0x9, { x + 1, y }, { x + 1, y + 1 });
+            return addSegment(type, 0xc, { x + 1, y + 1 }, { x, y + 1 });
+
+        case 0x9:
+            return addSegment(type, type, { x + 1, y }, { x + 1, y + 2 });
+
+        case 0xa: // 0xb + 0xe
+            addSegment(type, 0x3, { x + 1, y }, { x + 2, y });
+            addSegment(type, 0x9, { x + 2, y }, { x + 2, y + 1 });
+            addSegment(type, 0xc, { x + 1, y + 2 }, { x, y + 2 });
+            return addSegment(type, 0x6, { x, y + 2 }, { x, y + 1 });
+
+        case 0xb:
+            addSegment(type, 0x3, { x + 1, y }, { x + 2, y });
+            return addSegment(type, 0x9, { x + 2, y }, { x + 2, y + 1 });
+
+        case 0xc:
+            return addSegment(type, type, { x + 2, y + 1 }, { x, y + 1 });
+
+        case 0xd:
+            addSegment(type, 0x9, { x + 2, y + 1 }, { x + 2, y + 2 });
+            return addSegment(type, 0xc, { x + 2, y + 2 }, { x + 1, y + 2 });
+
+        case 0xe:
+            addSegment(type, 0xc, { x + 1, y + 2 }, { x, y + 2 });
+            return addSegment(type, 0x6, { x, y + 2 }, { x, y + 1 });
+
+        case 0xf: return;
+        }
+    }
+
+    math::Points2 extract(const Ring *ring) {
+        LOG(info4) << "Processing ring starting at: " << ring->head;
         math::Points2 points;
 
-        SegmentIndex si(start);
+        const Segment *s(ring->head);
         do {
-            LOG(info4) << "    adding: " << si;
-            const auto &s(segments[si]);
-            if (s.ring != start) {
+            LOG(info4) << "    adding: " << s;
+
+            if (s->links.ring.get() != ring) {
                 LOGTHROW(info4, std::runtime_error)
-                    << "Segment: " << si << " doesn't belong to ring "
-                    << start << " but " << s.ring << ".";
-            }
-            if (s.next < 0) {
-                LOGTHROW(info4, std::runtime_error)
-                    << "Segment: " << si << " in ring "
-                    << s.ring << " has no next segment.";
+                    << "Segment: " << s << " doesn't belong to ring "
+                    << ring << " but " << s->links.ring << ".";
             }
 
-            points.emplace_back(s.start(0) / 2.0, s.start(1) / 2.0);
-            si = s.next;
-        } while (si != start);
+            if (!s->links.next) {
+                LOGTHROW(info4, std::runtime_error)
+                    << "Segment: type: [" << std::bitset<4>(s->fullType) << "/"
+                    << std::bitset<4>(s->type) << "] "
+                    << "<" << s->start << " -> " << s->end << "> "
+                    << s << " in ring "
+                    << s->links.ring << " has no next segment.";
+            }
+
+            points.emplace_back(s->start(0) / 2.0, s->start(1) / 2.0);
+            s = s->links.next;
+        } while (s != ring->head);
 
         return points;
     }
@@ -211,20 +366,17 @@ struct ContourBuilder {
     Contours extract() {
         Contours contours;
 
-        SegmentIndex ring(-1);
-        for (SegmentIndex si(0), se(segments.size()); si < se; ++si) {
-            const auto &s(segments[si]);
-            if (s.ring == ring) { continue; }
-            contours.push_back(extract(si));
-            ring = si;
+        LOG(info4) << "Rings: " << rings.size();
+
+        for (const auto &ring : rings) {
+            contours.push_back(extract(ring));
         }
 
         return contours;
     }
 
-    Segment::list segments;
-    Segment::map segmentsByStart;
-    Segment::map segmentsByEnd;
+    Ring::set rings;
+    SegmentMap segments;
 };
 
 } // namespace
@@ -242,14 +394,42 @@ Contours findContours(const bitfield::RasterMask &mask)
         return (mask.get(x, y) ? flag : 0);
     });
 
-    for (int j(-1), je(size.height); j != je; ++j) {
-        for (int i(-1), ie(size.width); i != ie; ++i) {
-            cb.add(i, j, getFlag(i, j + 1, 1)
-                   | getFlag(i + 1, j + 1, 2)
-                   | getFlag(i + 1,  j, 4)
-                   | getFlag(i, j, 8));
-        }
+    const auto add([&](int x, int y)
+    {
+        cb.add(x, y, getFlag(x, y + 1, 1)
+               | getFlag(x + 1, y + 1, 2)
+               | getFlag(x + 1,  y, 4)
+               | getFlag(x, y, 8));
+    });
+
+    const auto addBorder([&](int x, int y)
+    {
+        cb.addBorder(x, y, getFlag(x, y + 1, 1)
+                     | getFlag(x + 1, y + 1, 2)
+                     | getFlag(x + 1,  y, 4)
+                     | getFlag(x, y, 8));
+    });
+
+    int xend(size.width - 1);
+    int yend(size.height - 1);
+
+    // first row
+    for (int i(-1); i <= xend; ++i) { addBorder(i, -1); }
+
+    // all inner rows
+    for (int j(0); j < yend; ++j) {
+        // first column
+        addBorder(-1, j);
+
+        // all inner columns
+        for (int i(0); i < xend; ++i) { add(i, j); }
+
+        // last column
+        addBorder(xend, j);
     }
+
+    // last row
+    for (int i(-1); i <= xend; ++i) { addBorder(i, yend); }
 
     return cb.extract();
 }
