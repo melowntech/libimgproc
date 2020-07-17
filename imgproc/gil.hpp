4e2ec606c28064069a5d6eaf9e5d0d2e8987e762
Vaclav Blazek <vaclav.blazek@gmail.com>
2020-07-17 18:19:26 +0200

    compat: Boost>=1.69, OpenCV-4

--- imgproc/gil.hpp
+++ imgproc/gil.hpp
@@ -24,45 +24,15 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef imgproc_cvcompat_hpp_included_
-#define imgproc_cvcompat_hpp_included_
+#ifndef imgproc_gil_hpp_included_
+#define imgproc_gil_hpp_included_
 
-#include "opencv2/core/version.hpp"
+#include <boost/version.hpp>
 
-#if CV_VERSION_MAJOR >= 4
-
-// cv::cvtColor constants
-#define IMGPROC_CVT_COLOR(what) cv::COLOR_##what
-
-// img codec constants (imread)
-#define IMGPROC_IMREAD(what) cv::IMREAD_##what
-
-// contours
-#define IMGPROC_CONTOURS(what) cv::what
-
-// distance
-#define IMGPROC_DISTANCE(what) cv::what
-
-// Hersey font
-#define IMGPROC_FONT_HERSHEY(what) cv::HersheyFonts::FONT_HERSHEY_##what
-
+#if BOOST_VERSION < 106900
+#  include <boost/gil/gil_all.hpp>
 #else
-
-// cv::cvtColor constants
-#define IMGPROC_CVT_COLOR(what) CV_##what
-
-// img codec constants (imread)
-#define IMGPROC_IMREAD(what) CV_LOAD_IMAGE_##what
-
-// contours
-#define IMGPROC_CONTOURS(what) CV_##what
-
-// distance
-#define IMGPROC_DISTANCE(what) CV_##what
-
-// Hersey font
-#define IMGPROC_FONT_HERSHEY(what) CV_FONT_HERSHEY_##what
-
+#  include <boost/gil.hpp>
 #endif
 
-#endif // imgproc_cvcompat_hpp_included_
+#endif // imgproc_gil_hpp_included_
